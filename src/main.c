625132ef169f40629c57e3c1a4fb0470a86292e8
Kinyarasam <skinyara.30@gmail.com>
2024-11-06 18:01:09 +0300

    Develop (#2)
    * Setup build system with CMake and initial main.c
    
    * Setup build system with CMake and initial main.c
    
    * Update .gitignore
    
    * CMakeLists.txt
    
    * Betty setup
    
    * changes
    
    * changes

--- src/main.c
+++ src/main.c
@@ -4,7 +4,15 @@
 #include <lualib.h>
 #include <lauxlib.h>
 
-int main(int argc, char* argv[]) {
-    printf("Mission: Love Ops Initialized!\n");
-    return 0;
+/**
+ * main - Entry point.asm
+ *
+ * Return: Always 0
+ *          otherwise 1.
+ */
+int main(void)
+{
+	printf("Mission: Love Ops Initialized!\n");
+	printf("Mission: Love Ops Initialized!\n");
+	return (0);
 }
