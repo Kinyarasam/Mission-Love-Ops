525bbf950e66c1255aa1b8b3410e17729ab0033a
kinyarasam <skinyara.30@gmail.com>
2024-11-06 16:05:20 +0300

    Setup build system with CMake and initial main.c

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+int main(int argc, char* argv[]) {
+    printf("Mission: Love Ops Initialized!\n");
+    return 0;
+}
