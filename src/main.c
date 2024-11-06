3d08629a0ff55419e9edf99606e62fdd8359370d
kinyarasam <skinyara.30@gmail.com>
2024-11-06 18:00:00 +0300

    Set up basic game states for menu and gameplay

--- src/main.c
+++ src/main.c
@@ -4,6 +4,15 @@
 #include <lualib.h>
 #include <lauxlib.h>
 
+typedef enum {
+    GAME_STATE_MENU,
+    GAME_STATE_PLAY,
+    GAME_STATE_EXIT
+} GameState;
+
+void handle_menu_state(SDL_Renderer *renderer);
+void handle_play_state(SDL_Renderer *renderer);
+
 /**
  * main - Entry point.
  *
@@ -12,8 +21,7 @@
  */
 int main(void)
 {
-	if (SDL_Init((SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0))
-	{
+	if (SDL_Init((SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)) {
 		fprintf(stderr, "SDL Initialization error: %s\n", SDL_GetError());
 		return (1);
 	}
@@ -21,8 +29,7 @@ int main(void)
 	SDL_Window * window = SDL_CreateWindow("Mission: Love Ops",
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 		800, 600, SDL_WINDOW_SHOWN);
-	if (!window)
-	{
+	if (!window) {
 		fprintf(stderr, "SDL Window Error: %s\n", SDL_GetError());
 		SDL_Quit();
 		return (1);
@@ -32,8 +39,7 @@ int main(void)
 		window,
 		-1,
 		SDL_RENDERER_ACCELERATED);
-	if (!renderer)
-	{
+	if (!renderer) {
 		fprintf(stderr, "SDL Renderer Error: %s\n", SDL_GetError());
 		SDL_DestroyWindow(window);
 		SDL_Quit();
@@ -46,27 +52,52 @@ int main(void)
 
 	SDL_Event e;
 	int quit = 0;
+	GameState gameState = GAME_STATE_MENU;
+
 
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
 
-	if (luaL_dofile(L, "scripts/test_script.lua") != LUA_OK) {
-		printf("Lua Error: %s\n", lua_tostring(L, -1));
-		lua_close(L);
-		SDL_DestroyRenderer(renderer);
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return 1;
-	}
+	// if (luaL_dofile(L, "scripts/test_script.lua") != LUA_OK) {
+	// 	printf("Lua Error: %s\n", lua_tostring(L, -1));
+	// 	lua_close(L);
+	// 	SDL_DestroyRenderer(renderer);
+	// 	SDL_DestroyWindow(window);
+	// 	SDL_Quit();
+	// 	return 1;
+	// }
 
-	while (!quit)
-	{
-		while (SDL_PollEvent(&e))
-		{
-			if (e.type == SDL_QUIT)
+	while (!quit) {
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT) {
 				quit = 1;
+			}
+			if (e.type == SDL_KEYDOWN) {
+				switch (e.key.keysym.sym) {
+					case SDLK_m:
+						gameState = GAME_STATE_MENU;
+						break;
+					case SDLK_p:
+						gameState = GAME_STATE_PLAY;
+						break;
+					case SDLK_q:
+						gameState = GAME_STATE_EXIT;
+						break;
+				}
+			}
 		}
-	}
+		switch (gameState) {
+			case GAME_STATE_MENU:
+				handle_menu_state(renderer);
+				break;
+			case GAME_STATE_PLAY:
+				handle_play_state(renderer);
+				break;
+			case GAME_STATE_EXIT:
+				quit = 1;
+				break;
+		}
+    }
 
 	lua_close(L);
 
@@ -75,4 +106,18 @@ int main(void)
 	SDL_Quit();
 
 	return (0);
+}
+
+void handle_menu_state(SDL_Renderer *renderer) {
+    // Render menu (this can be a placeholder for now)
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // Blue background for menu
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
+
+void handle_play_state(SDL_Renderer *renderer) {
+    // Render gameplay (for now, just a simple placeholder)
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Green background for gameplay
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 }
